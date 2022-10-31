5a6fa8b25a682a407be41b7d5975835096a40219
Adriana Lima <adrianalima_webdesigner@outlook.com>
2022-10-31 12:00:26 -0300

    Update Repo.js

--- src/Repo.js
+++ src/Repo.js
@@ -5,12 +5,12 @@ const Repo = ({ repo }) =>(
     <div className="card card-header mb-2">
         <div className="row">
             <div className="col-md-6">
-                <a>{Repo.name} </a>
+                <a>{repo.name} </a>
             </div>
             <div className="col-md-6 ">
-                <button className="badge badge-primary">Stars: { Repo.stargazers_count }</button>
-                <button className="badge badge-secondary">Watch: { Repo.watchers_count }</button>
-                <button className="badge badge-sucess">Forks: { Repo.forks_count }</button>
+                <button className="badge badge-primary">Stars: { repo.stargazers_count }</button>
+                <button className="badge badge-secondary">Watch: { repo.watchers_count }</button>
+                <button className="badge badge-sucess">Forks: { repo.forks_count }</button>
             </div>
         </div>
     </div>
